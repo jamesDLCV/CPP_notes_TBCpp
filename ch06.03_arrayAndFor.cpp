715176fada2a192a46c04dbb8479b09d1b526b0a
jamesDLCV <69728729+jamesDLCV@users.noreply.github.com>
2021-02-10 17:40:15 +0800

    Update ch06.03_arrayAndFor.cpp
    line 8, you can do it like this.  
    line 18, 19, why do they have different values?

--- ch06.03_arrayAndFor.cpp
+++ ch06.03_arrayAndFor.cpp
@@ -5,7 +5,7 @@ int main()
 {
     //const int num_students = 5;
     int scores[] = { 84, 92, 76, 81, 56 };
-    const int num_students = sizeof(scores) / sizeof(int);
+    const int num_students = sizeof(scores) / sizeof(int);               // can do it like this. 
     int total_score = 0;
     int max_score = 0;
 
@@ -15,13 +15,13 @@ int main()
         max_score = ((max_score < scores[i]) ? scores[i] : max_score);
     }
         
-    double avg_score = static_cast<double>(total_score) / num_students;
-    double avg_score2 = (double)(total_score / num_students);
+    double avg_score = static_cast<double>(total_score) / num_students;  // 77.8
+    double avg_score2 = (double)(total_score / num_students);            // 77. it is different from avg_score
 
     cout << avg_score << endl;
     cout << avg_score2 << endl;
 
-    cout << max_score << endl;
+    cout << max_score << endl;                                           // 92
 
     return 0;
 }
