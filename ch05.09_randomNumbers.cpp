0b75d6a93f88852dcbe12b48c7700c70ee6eabe8
jamesDLCV <69728729+jamesDLCV@users.noreply.github.com>
2021-03-03 18:42:59 +0800

    random numbers

---
+++ ch05.09_randomNumbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+unsigned int PRNG() // Pseudo Random Number
+{
+    static unsigned int seed = 5523; // seed number
+    seed = 8253729 * seed + 2396403;
+
+    return seed % 32768;
+}
+
+int main()
+{
+    for (int count = 1; count <= 100; ++count)
+    {
+        cout << PRNG() << "\t";
+        if (count % 5 == 0) cout << endl;
+    }
+
+    return 0;
+}
